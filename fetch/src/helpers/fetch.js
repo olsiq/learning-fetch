972cbad9d663b500281b1e16f7a13054424ae9b5
olsiq <64708189+olsiq@users.noreply.github.com>
2022-04-11 02:14:18 +0300

    done

--- fetch/src/helpers/fetch.js
+++
@@ -1,6 +0,0 @@
-export const letsFetchAdvice = async (url) => {
-  const res = await fetch(url);
-  const response = await res.json();
-  const list = await response.slip.advice;
-  return list;
-};
